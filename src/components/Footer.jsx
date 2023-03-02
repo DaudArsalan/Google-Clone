5f38050fbb816616e0efdd7962116659ed215315
Daud <daudarslan869@gmail.com>
2023-03-02 19:04:08 +0500

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import { quickLinks, settingMenu } from "../utils/constants";
+import { quickLinks, settingMenu } from "../utils/Constants";
 
 const Footer = () => {
   return (
     <footer className="bg-[#f2f2f2]">
       <div className="flex py-[15px] px-[15px] md:px-[30px] border-b border-[#dadce0]">
-        <span className="text-[#70757a] text-[15px] leading-none">Pakistan</span>
+        <span className="text-[#70757a] text-[15px] leading-none">
+          Pakistan
+        </span>
       </div>
 
       <div className="flex flex-col md:flex-row justify-between py-3 md:py-0 md:px-[15px] border-b border-[#dadce0]">
@@ -14,7 +16,7 @@ const Footer = () => {
           {quickLinks.map((menu, index) => (
             <span
               key={index}
-              className="text-[#70757a] text-[12px] md:text-[14px] leading-none p-[10px] md:p-[15px]" 
+              className="text-[#70757a] text-[12px] md:text-[14px] leading-none p-[10px] md:p-[15px]"
             >
               {menu}
             </span>
